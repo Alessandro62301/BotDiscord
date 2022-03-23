bbbb35f71e0ae1176099deacc4097164c302621a
Junior <alessandrojunior@MacBook-Air-de-Alessandro.local>
2022-03-22 23:52:39 -0300

    Upload

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const TOKEN = 'OTU2MDA0MDA1ODQ5NDc3MTUw.Yjp6HA.J6yNbHv3bJbCl0TNn2UPwtqp6-E';
+
+const TOKEN = process.env.TOKEN;
 
 client.on('ready', () => {
 	client.user.setStatus('your status')
